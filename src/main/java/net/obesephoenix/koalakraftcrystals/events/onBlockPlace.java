a7a2ddb8e2c07c25ad8c5021d810e74afba59453
Obesephoenix <john830beck@gmail.com>
2021-09-05 11:33:07 +0100

    Stopped player from placing crystals

--- src/main/java/net/obesephoenix/koalakraftcrystals/events/onBlockPlace.java
+++ src/main/java/net/obesephoenix/koalakraftcrystals/events/onBlockPlace.java
@@ -1,9 +1,6 @@
 package net.obesephoenix.koalakraftcrystals.events;
 
-import net.obesephoenix.koalakraftcrystals.crystals.Crystal;
 import net.obesephoenix.koalakraftcrystals.crystals.KKCrystalHandler;
-import org.bukkit.Sound;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
